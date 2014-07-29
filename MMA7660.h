98b0ae9fd8da029b5306b34bfc293ea2ccfc4fb0
lawliet zou <lawliet.zou@gmail.com>
2014-07-29 10:09:48 +0800

    version 1.2
    update README & file header

--- MMA7660.h
+++ MMA7660.h
@@ -1,27 +1,33 @@
-/*****************************************************************************/
-//	Function:    Header file for class MMC7660 
-//  Hardware:    Grove - 3-Axis Digital Accelerometer(±1.5g)
-//	Arduino IDE: Arduino-1.0
-//	Author:	 Frankie.Chu		
-//	Date: 	 Jan 10,2013
-//	Version: v0.9b
-//	by www.seeedstudio.com
-//
-//  This library is free software; you can redistribute it and/or
-//  modify it under the terms of the GNU Lesser General Public
-//  License as published by the Free Software Foundation; either
-//  version 2.1 of the License, or (at your option) any later version.
-//
-//  This library is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-//  Lesser General Public License for more details.
-//
-//  You should have received a copy of the GNU Lesser General Public
-//  License along with this library; if not, write to the Free Software
-//  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-//
-/*******************************************************************************/
+/*
+ * MMA7760.h
+ * Library for accelerometer_MMA7760
+ *
+ * Copyright (c) 2013 seeed technology inc.
+ * Author        :   FrankieChu 
+ * Create Time   :   Jan 2013
+ * Change Log    :
+ *
+ * The MIT License (MIT)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+ 
 #ifndef __MMC7660_H__
 #define __MMC7660_H__
 
@@ -35,30 +41,30 @@
 #define MMA7660_SPCNT 0x05
 #define MMA7660_INTSU 0x06
 #define MMA7660_MODE  0x07
-	#define MMA7660_STAND_BY 0x00
-	#define MMA7660_ACTIVE	 0x01	
-#define MMA7660_SR    0x08		//sample rate register
-	#define AUTO_SLEEP_120	0X00//120 sample per second
-	#define AUTO_SLEEP_64	0X01
-	#define AUTO_SLEEP_32	0X02
-	#define AUTO_SLEEP_16	0X03
-	#define AUTO_SLEEP_8	0X04
-	#define AUTO_SLEEP_4	0X05
-	#define AUTO_SLEEP_2	0X06
-	#define AUTO_SLEEP_1	0X07
+    #define MMA7660_STAND_BY 0x00
+    #define MMA7660_ACTIVE   0x01   
+#define MMA7660_SR    0x08      //sample rate register
+    #define AUTO_SLEEP_120  0X00//120 sample per second
+    #define AUTO_SLEEP_64   0X01
+    #define AUTO_SLEEP_32   0X02
+    #define AUTO_SLEEP_16   0X03
+    #define AUTO_SLEEP_8    0X04
+    #define AUTO_SLEEP_4    0X05
+    #define AUTO_SLEEP_2    0X06
+    #define AUTO_SLEEP_1    0X07
 #define MMA7660_PDET  0x09
 #define MMA7660_PD    0x0A
 class MMA7660
 {
 private:
-	void write(uint8_t _register, uint8_t _data);
-	uint8_t read(uint8_t _register);
+    void write(uint8_t _register, uint8_t _data);
+    uint8_t read(uint8_t _register);
 public:
-	void init();
-	void setMode(uint8_t mode);
-	void setSampleRate(uint8_t rate);
-	void getXYZ(int8_t *x,int8_t *y,int8_t *z);
-	void getAcceleration(float *ax,float *ay,float *az);
+    void init();
+    void setMode(uint8_t mode);
+    void setSampleRate(uint8_t rate);
+    void getXYZ(int8_t *x,int8_t *y,int8_t *z);
+    void getAcceleration(float *ax,float *ay,float *az);
 };
 
 #endif
--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-Grove_3Axis_Digital_Accelerometer_MMA7660
--------------------------------------------------------------
+##Accelerometer_MMA7660
 ![image](http://www.seeedstudio.com/wiki/images/b/bb/3_aix_acc.jpg)
 
 3-Axis Digital Accelerometer is the key part in projects like orientation detection, gesture detection and Motion detection. This 3-Asix Digital Accelerometer(±1.5g) is based on Freescale's low power consumption module, MMA7660FC. It features up to 10,000g high shock surviability and configurable Samples per Second rate. For generous applications that don't require too large measurement range, this is a great choice because it's durable, energy saving and cost-efficient.
@@ -15,14 +14,11 @@ Contributing to this software is warmly welcomed. You can do this basically by<b
 for operating guide). Adding change log and your contact into file header is encouraged.<br>
 Thanks for your contribution.
 
-Seeed Studio is an open hardware facilitation company based in Shenzhen, China. <br>
-Benefiting from local manufacture power and convenient global logistic system, <br>
-we integrate resources to serve new era of innovation. Seeed also works with <br>
-global distributors and partners to push open hardware movement.<br>
+Seeed is a hardware innovation platform for makers to grow inspirations into differentiating products. By working closely with technology providers of all scale, Seeed provides accessible technologies with quality, speed and supply chain knowledge. When prototypes are ready to iterate, Seeed helps productize 1 to 1,000 pcs using in-house engineering, supply chain management and agile manufacture forces. Seeed also team up with incubators, Chinese tech ecosystem, investors and distribution channels to portal Maker startups beyond.
 
 
 [1]:http://www.seeedstudio.com/wiki/Grove_-_3-Axis_Digital_Accelerometer(%C2%B11.5g)
 
 
 
-[![Analytics](https://ga-beacon.appspot.com/UA-46589105-3/Grove_3Axis_Digital_Accelerometer_MMA7660)](https://github.com/igrigorik/ga-beacon)
+[![Analytics](https://ga-beacon.appspot.com/UA-46589105-3/Accelerometer_MMA7660)](https://github.com/igrigorik/ga-beacon)
